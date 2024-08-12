7db51741cd3b89f839a545c606781e66f004883b
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2024-08-12 17:53:54 +0700

    add: server

--- scripts/server.py
+++ scripts/server.py
@@ -6,10 +6,10 @@ import argparse
 
 load_dotenv()
 
-url = os.getenv('DATABASE_URL')
+URL = os.getenv('DATABASE_URL')
 KEY = os.getenv('DB_KEY')
 
-def post_articles(url, jsonfile):
+def post_articles(jsonfile):
   with open(f'./data/{jsonfile}.json', 'r') as f:
     articles = json.load(f)
 
@@ -18,14 +18,14 @@ def post_articles(url, jsonfile):
     'Content-Type': 'application/json'
   }
 
-  response = requests.post(url + '/articles/list', json=articles, headers=headers)
+  response = requests.post(URL + '/articles/list', json=articles, headers=headers)
 
   if response.status_code == 200:
     print('Success:', response.json())
   else:
     print('Failed:', response.status_code, response.text)
 
-def post_article(url, jsonfile):
+def post_article(jsonfile):
   with open(f'./data/{jsonfile}.json', 'r') as f:
     article = json.load(f)
 
@@ -37,27 +37,73 @@ def post_article(url, jsonfile):
     'Content-Type': 'application/json'
   }
 
-  response = requests.post(url + '/articles', json=article, headers=headers)
+  response = requests.post(URL + '/articles', json=article, headers=headers)
 
   if response.status_code == 200:
     print('Success:', response.json())
   else:
     print('Failed:', response.status_code, response.text)
 
+def post_source(jsonfile):
+  with open(f'./data/{jsonfile}.json', 'r') as f:
+    articles = json.load(f)
+
+  headers = {
+    'Authorization': f'Bearer {KEY}',
+    'Content-Type': 'application/json'
+  }
+  
+  
+  if isinstance(articles, list):
+    
+    all_articles_db = requests.get(URL + '/articles', headers=headers).json()
+    links = []
+    for article_db in all_articles_db:
+      if article_db['source'] not in links:
+        links.append(article_db['source'])
+    # print(links)
+    
+    submit_article = []
+    for article in articles:
+      if article['source'] not in links:
+        response = requests.post(URL + '/url-article', json=article, headers=headers)
+    
+
+        if response.status_code == 200:
+          print('Inference Success:', response.json()['source'])
+          submit_article.append(response.json())
+          links.append(article['source'])
+        else:
+          print('Inference Failed:', response.status_code, response.text)
+    # print(submit_article)
+      else:
+        print("Article already exists")
+    
+    response = requests.post(URL + '/articles/list', json=submit_article, headers=headers)
+    if response.status_code == 200:
+      print('Success:', response.json())
+      submit_article.append(response.json())
+    else:
+      print('Failed:', response.status_code, response.text)
+        
+
 def main():
 
   parser = argparse.ArgumentParser(description="Script for posting articles to database server")
   parser.add_argument("filename", type=str, default="labeled_articles")
   parser.add_argument("--list", action='store_true', help="Flag to indicate posting a list of articles")
+  parser.add_argument("--i", action='store_true', help="Flag to use LLM inferencing")
 
   args = parser.parse_args()
 
   filename = args.filename
-
-  if args.list:
-    post_articles(url, filename)
+  
+  if args.i:
+    post_source(filename)
+  elif args.list:
+    post_articles(filename)
   else:
-    post_article(url, filename)
+    post_article(filename)
 
 if __name__ == "__main__":
   main()
