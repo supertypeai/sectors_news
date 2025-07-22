d31b6a29281bec36d23ae76a68ef50db5adb6e71
Fawwaz Atha Rohmatullah <132468764+fawazatha@users.noreply.github.com>
2025-07-22 13:05:59 +0700

    Update adding more key models

--- config/setup.py
+++ config/setup.py
@@ -1,7 +1,9 @@
-import logging
-from dotenv import load_dotenv
-import os 
+from dotenv     import load_dotenv
+from threading  import Semaphore
 
+import os 
+import asyncio
+import logging
 
 logging.basicConfig(
     # filename='app.log', # Set a file for save logger output 
@@ -19,3 +21,9 @@ load_dotenv(override=True)
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GROQ_API_KEY1 = os.getenv("GROQ_API_KEY1")
+GROQ_API_KEY2 = os.getenv("GROQ_API_KEY2")
+GROQ_API_KEY3 = os.getenv("GROQ_API_KEY3")
+
+_LLM_SEMAPHORE_SYNC = Semaphore(5)
+_LLM_SEMAPHORE = asyncio.Semaphore(5)
